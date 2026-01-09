0a9e225615205be5c06c6f979c4df9bf876d9fb6
quunnb <quunnb@amideus.fi>
2026-01-09 05:07:34 +0200

    Add patches, change colors

--- config.def.h
+++ config.def.h
@@ -1,24 +1,26 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
+static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;  /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"ShureTechMono Nerd Font:size=16"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = {"DepartureMono Nerd Font:size=16"};
+static const char *prompt = "> "; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*                 fg         bg       */
-    [SchemeNorm] = { "#FFFFFF", "#000000" },
-    [SchemeSel]  = { "#FF0000", "#000000" },
-    [SchemeOut]  = { "#FFFFFF", "#000000" },
+    /*                  fg         bg       */
+    [SchemeNorm] =   { "#74888C", "#D5D4C0" },
+    [SchemeSel] =    { "#45575B", "#D5D4C0" },
+    [SchemeOut] =    { "#74888C", "#D5D4C0" },
+    [SchemeBorder] = { "#74888C", "#74888C" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 3;
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 4;
